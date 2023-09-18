b7489f4d9929366e17bdb513761b6a11ed1ac1e1
Anshulsharma63 <141258794+Anshulsharma63@users.noreply.github.com>
2023-09-18 21:41:18 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,22 @@
+var http = require('http')
+
+var fs = require('fs')
+
+
+http.createServer(function (req,res){
+
+fs.readFile('index.html', function(err, data){
+
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    res.write(data);
+
+    return res.end();
+       
+});
+
+
+}).listen(90, ()=>{
+    console.log("listing to port 90")
+})
